71f7d066de8489b575f9acb2a9ae54b5987dc7e5
Tara <>
2025-08-07 13:50:52 -0700

    Removing unecessary lines and restoring new lines

--- sensor_simulator.py
+++ sensor_simulator.py
@@ -34,10 +34,6 @@ def get_opts():
     # parser.add_argument("--interval", type=float, default=30.0,
     #                     help="Interval in seconds for min, max, average aggregation.")
 
-    #parser.add_argument("-u", "--username", default=utils.get_gridappsd_user(),
-    #                    help="The username to authenticate with the message bus.")
-    #parser.add_argument("-p", "--password", default=utils.get_gridappsd_pass(),
-    #                    help="The password to authenticate with the message bus.")
     parser.add_argument("-a", "--address", default=utils.get_gridappsd_address(),
                         help="The tcp://addr:port that gridappsd is located on.")
     opts = parser.parse_args()
@@ -83,6 +79,7 @@ if __name__ == '__main__':
         if configs['id'] == 'gridappsd-sensor-simulator':
             user_options = configs['user_options']
             break
+        
     feeder = opts.request['power_system_config']['Line_name']
     service_id = "gridappsd-sensor-simulator"
     gapp = GridAPPSD(address=opts.address)
@@ -90,6 +87,7 @@ if __name__ == '__main__':
     #gapp.get_logger().setLevel(opts.log_level)
     read_topic = simulation_output_topic(opts.simulation_id)
     write_topic = service_output_topic(service_id, opts.simulation_id)
+
     log_file = "/tmp/gridappsd_tmp/{}/sensors.log".format(opts.simulation_id)
     if not os.path.exists(os.path.dirname(log_file)):
         os.makedirs(os.path.dirname(log_file))
@@ -100,6 +98,7 @@ if __name__ == '__main__':
 
     meas = Measurements()
     meta = meas.get_sensors_meta(feeder)
+
     with open(log_file, 'w') as fp:
         logging.basicConfig(stream=fp, level=logging.INFO)
         logging.getLogger().info("Almost ready to create sensors!")
